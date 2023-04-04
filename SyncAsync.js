4a4d0c4b1dc2415d782d568154d00e0235b28830
Dipra <dipradas5940@gmail.com>
2023-04-04 20:32:48 +0600

    Sync Async

---
+++ SyncAsync.js
@@ -0,0 +1,8 @@
+console.log("Welcome to ACC");
+setTimeout(() => console.log("I am excited to tech"), 3000);
+console.log("Hope u are also excited");
+
+
+// multiple async function normally use korte gele ekta async function er vetor r ekta async function dhukte thake 
+// and same process er jonnno loop hoye colte thake
+// Known as callback hell!
